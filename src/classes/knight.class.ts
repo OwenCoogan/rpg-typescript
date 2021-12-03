66d96e675494001b55f3baa47ade51b80c7b6e0e
Owen <owen.coogan@myjobglasses.com>
2021-12-03 11:55:09 +0100

    first commit

--- src/classes/knight.class.ts
+++ src/classes/knight.class.ts
@@ -1,12 +1,5 @@
-export default class Champion {
 
-  name:string;
-  lifepoints:number;
-  constructor(name: string,lifepoints:number) {
-    this.name = name;
-    this.lifepoints = lifepoints;
-  }
-  attack(){
+export default class Knight extends Champion{
 
-  }
+
 }
