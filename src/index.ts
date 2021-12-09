6a2ed37d45eed43b8f8526286c48864d247afdde
Owen <owen.coogan@myjobglasses.com>
2021-12-10 00:30:10 +0100

    adding refacto to index

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,10 @@ let newKnight : Knight  = new Knight("DumbDumb",20,6)
 let newArcher : Archer  = new Archer("Badshot",20,5)
 let newMage : Mage  = new Mage("Gandoulfe",20,4)
 
+let i= 1;
 
 function fightRound(roundNumber:number) {
-
   console.log(`Fight Night : this is round ${roundNumber}`);
-
-  newKnight.attack(newArcher);
-  newArcher.timedOut === true ? newArcher.attack(newMage) : newArcher.doubleTap(newMage)
-  newMage.attack(newKnight);
 
   if(newKnight.protected === false && newKnight.timedOut === false ){
     newKnight.protect();
@@ -22,6 +18,18 @@ function fightRound(roundNumber:number) {
   if(newMage.lifepoints > 0 && newMage.timedOut === false){
     newMage.heal();
   }
+  const champions = [newKnight,newArcher,newMage];
+
+    champions.forEach(champion => {
+      if(i===3){
+        champion.attack(champions[0]);
+        i=0;
+      }
+      else{
+        champion.attack(champions[i]);
+        i=i+1;
+      }
+    });
 }
 
 const rounds = [1,2,3,4,5]
