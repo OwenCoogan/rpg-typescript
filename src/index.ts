07173482fc21c7e0d8f83793909b3e9a127be6f7
Owen <owen.coogan@myjobglasses.com>
2021-12-10 09:19:09 +0100

    adding refacto to index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Archer from './classes/archer.class'
 import Mage from './classes/mage.class'
 
 let newKnight : Knight  = new Knight("DumbDumb",20,6)
+let newKnight1 : Knight  = new Knight("BumBum",20,6)
 let newArcher : Archer  = new Archer("Badshot",20,5)
 let newMage : Mage  = new Mage("Gandoulfe",20,4)
 
@@ -14,14 +15,13 @@ function fightRound(roundNumber:number) {
   if(newKnight.protected === false && newKnight.timedOut === false ){
     newKnight.protect();
   }
-
   if(newMage.lifepoints > 0 && newMage.timedOut === false){
     newMage.heal();
   }
-  const champions = [newKnight,newArcher,newMage];
+  const champions = [newKnight,newArcher,newMage,newKnight1];
 
     champions.forEach(champion => {
-      if(i===3){
+      if(i===champions.length){
 
         champion.attack(champions[0]);
         i=0;
